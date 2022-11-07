7bc53a64a128f08cfeb5e03941101446413dd413
Edward Whitehead <edward@ultracoolbru.com>
2022-11-07 22:16:43 +0300

    Minted and completed
    https://testnets.opensea.io/assets/goerli/0xebbe1a29ac5af49d09746485f9e688ed7e160bb9/0

---
+++ contracts/PayBeeNft.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts@4.7.3/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.7.3/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts@4.7.3/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.7.3/utils/Counters.sol";
+
+contract PayBee is ERC721, ERC721Enumerable, ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    uni256 MAX_SUPPLY = 10000;
+    mapping (address => uint) totalNumberOfNftAllowedToMint;
+
+    constructor() ERC721("PayBee", "PBNFT") {}
+
+    function safeMint(address to, string memory uri) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        require(tokenId < MAX_SUPPLY, "Sorry, all our bees are gone and we can't make more right now.");
+        require(totalNumberOfNftAllowedToMint[msg.sender] < 10, "You can't mint more than 10 NFTs.");
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
